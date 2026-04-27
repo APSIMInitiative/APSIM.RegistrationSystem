11af4b85b1f73dd196bf2f86f70cab1b22ccab0a
Julian Rich <123442863+ric394@users.noreply.github.com>
2026-04-27 16:07:11 +1000

    allow product boxes and its object to have a second href and text

--- RegistrationWebApp/Components/LayoutObjects/Product.cs
+++ RegistrationWebApp/Components/LayoutObjects/Product.cs
@@ -8,4 +8,8 @@ public class Product
     public required string ImageUrl { get; set; }
 
     public required string Href { get; set; }
+    public required string FirstButtonText { get; set; }
+
+    public string SecondHref { get; set; } = string.Empty;
+    public string SecondButtonText { get; set; } = string.Empty;
 }
