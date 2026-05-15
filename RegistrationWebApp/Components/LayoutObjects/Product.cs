b6b9a49a5cbac0fb0da6fde8c6b0c9c854b14c8d
Julian Rich <julian.rich@csiro.au>
2026-05-15 14:31:07 +1000

    home page improvements to allow better legibility of text and to improve the written content as well, this also improves alignment of button elements

--- RegistrationWebApp/Components/LayoutObjects/Product.cs
+++ RegistrationWebApp/Components/LayoutObjects/Product.cs
@@ -1,10 +1,12 @@
 
+using Microsoft.AspNetCore.Components;
+
 namespace RegistrationWebApp.Components.LayoutObjects;
 
 public class Product
 {
     public required string Name { get; set; }
-    public required string Description { get; set; }
+    public required MarkupString Description { get; set; }
     public required string ImageUrl { get; set; }
 
     public required string Href { get; set; }
